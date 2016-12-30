3c2b477827dc7edd2cc339ee7be4882647e16e4e
spezi77 <spezi77@gmx.com>
2016-12-30 10:43:55 +0100

    Update path for wpa_supplicant.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- wifi/device.mk
+++ wifi/device.mk
@@ -8,7 +8,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	device/linaro/hikey/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+	device/linaro/generic/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 		wifi.interface=wlan0 \
